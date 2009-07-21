246f57d69b2007de953827d7c75c64ad8534f2a7
Kent Fredric <kentfredric+gravitar@gmail.com>
2009-07-22 09:11:53 +1200

    +Documenting, Minor refactor

--- lib/Catalyst/Authentication/Credential/RPX.pm
+++ lib/Catalyst/Authentication/Credential/RPX.pm
@@ -10,20 +10,89 @@ use MooseX::Has::Sugar;
 use namespace::autoclean;
 use Net::API::RPX;
 
-has '_config'        => ( isa => HashRef, rw, required, );
-has '_app'           => ( isa => Object,  rw, required, );
-has '_realm'         => ( isa => Object,  rw, required, );
-has 'api_key'        => ( isa => Str,     rw, required, );
-has 'base_url'       => ( isa => Str,     rw, predicate => 'has_base_url', );
-has 'ua'             => ( isa => Str,     rw, predicate => 'has_ua', );
-has 'token_field'    => ( isa => Str,     rw, default => 'token' );
-has 'last_auth_info' => ( isa => HashRef, rw, predicate => 'has_last_auth_info', clearer => 'clear_last_auth_info' );
+=head1 SYNOPSIS
+
+    use Catalyst qw/ Authentication /;
+
+    package MyApp::Controller::Auth; 
+
+    sub login : Local { 
+        my ( $self , $c ) = @_; 
+        $c->authenticate();
+    }
+
+=head1 CONFIGURATION
+
+    __PACKAGE__->config->{'Plugin::Authenticate'} = {
+      default_realm => 'RPX_Service',
+      realms        => {
+        RPX_Service => {
+          credential => {
+            class => 'RPX',
+
+            # Package Options
+            api_key => 'ASDF...',
+
+            # optional fields
+            base_url    => 'http://foo.bar.org',
+            ua          => 'Firefox',
+            token_field => 'token',
+          }
+        }
+      }
+    };
+
+=cut
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item * C<api_key>  | C< ro required Str >
+
+The API Key for connecting to the RPX server.
+
+=item * C<base_url>  | C< ro Str predicate=has_base_url >
+
+The URL The RPX server interconnects with.
+
+=item * C<ua> | C< ro Str predicate=has_ua >
+
+The User-Agent String.
+
+=item * C<token_field> | C< ro Str default='token' >
+
+The token to look for in request params 
+
+=item * C<last_auth_info> | C< rw HashRef predicate=has_last_auth_info  clearer=clear_last_auth_info >
+
+The results of the last call to C<< ->auth_info >>
+
+=back 
+
+=cut
+
+has '_config'     => ( isa => HashRef, rw, required, );
+has '_app'        => ( isa => Object,  rw, required, );
+has '_realm'      => ( isa => Object,  rw, required, );
+has 'api_key'     => ( isa => Str,     ro, required, );
+has 'base_url'    => ( isa => Str,     ro, predicate => 'has_base_url', );
+has 'ua'          => ( isa => Str,     ro, predicate => 'has_ua', );
+has 'token_field' => ( isa => Str,     ro, default => 'token' );
+
+has 'last_auth_info' => (
+  rw,
+  isa       => HashRef,
+  init_arg  => undef,
+  predicate => 'has_last_auth_info',
+  clearer   => 'clear_last_auth_info',
+);
 
 has '_api_driver' => (
   lazy_build, ro,
   isa      => Object,
   init_arg => undef,
-  handles  => [qw( map unmap mappings )],
+  handles  => [qw( auth_info map unmap mappings )],
 );
 
 sub BUILDARGS {
@@ -65,27 +134,23 @@ sub authenticate {
 
   $token = $c->req->params->{$token_field};
 
-  my $result = $self->auth_info({ token => $token });
+  my $result = $self->authenticate_rpx( { token => $token } );
 
-  if ( exists $result->{'err'} ){ 
-      return undef;
+  if ( exists $result->{'err'} ) {
+    return undef;
   }
-  
-  return $result; 
+
+  return $result;
 
 }
-sub auth_info { 
-    my $self = shift; 
-    return $self->_api_driver->auth_info( @_ );
-}
-around 'auth_info' => sub {
-  my $orig = shift;
+
+sub authenticate_rpx {
   my $self = shift;
   $self->clear_last_auth_info if $self->has_last_auth_info;
-  my $result = $self->$orig(@_);
+  my $result = $self->_api_driver->auth_info(@_);
   $self->last_auth_info($result);
   return $result;
-};
+}
 
 1;
 
